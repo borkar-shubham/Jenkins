3859b72bbfcb0075f115390a8a4b161bd5af785b
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-22 14:03:12 +0530

    Update pipeline_1.groovy

--- pipeline_1.groovy
+++ pipeline_1.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     stages {
-        stage('Build') {
+        stage('CloneGitRepo') {
             steps {
                 sh 'make' 
                 archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
