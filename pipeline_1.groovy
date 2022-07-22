94cedde11e7e728544f949e5cbf59d38c1d38e05
Shubham Borkar <shubham_borkar@outlook.com>
2022-07-22 15:22:47 +0530

    Update pipeline_1.groovy

--- pipeline_1.groovy
+++ pipeline_1.groovy
@@ -4,18 +4,27 @@ pipeline {
     stages {
         stage('CloneGitRepo') {
             steps {
-                sh 'make' 
-                archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true 
-            }
+                git branch: 'main', credentialsId: '90f4b6e9-c022-475d-8c83-559f3436dff7', url: 'https://github.com/borkar-shubham/Private_1.git'
+                    }
         }
-        stage('Test') {
+        stage('MavenCompile') {
+            steps {
+                sh 'mvn compile'
+                    }
+        }
+        stage('MvnTest') {
             steps {
                 /* `make check` returns non-zero on test failures,
                 * using `true` to allow the Pipeline to continue nonetheless
                 */
-                sh 'make check || true' 
-                junit '**/target/*.xml' 
+                sh 'mvn test'
             }
+        }
+        stage('MavenBuild') {
+            steps {
+                git branch: 'main', credentialsId: '90f4b6e9-c022-475d-8c83-559f3436dff7', url: 'https://github.com/borkar-shubham/Private_1.git'
+                sh 'mvn package'
+                    }
         }
         stage('Deploy') {
             when {
@@ -24,7 +33,13 @@ pipeline {
               }
             }
             steps {
-                sh 'make publish'
+                sh 'rm -rf docker-images'
+                sh 'mkdir docker-images'
+                sh 'rm -rf docker-images'
+                sh 'cd docker-images'
+                sh 'cp /var/lib/jenkins/workspace/*/*/target/*{.war,.jar} .'
+                sh 'touch dockerfile'
+                sh 'cat <<EOT>>dockerfile<<EOT
             }
         }
     }
