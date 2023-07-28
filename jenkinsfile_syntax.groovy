89e57fb7866a72e99e9d74fdd8afbdd3ead6f557
Shubham Borkar <shubham_borkar@outlook.com>
2023-07-28 21:22:34 +0530

    Update jenkinsfile_syntax.groovy

--- jenkinsfile_syntax.groovy
+++ jenkinsfile_syntax.groovy
@@ -1,9 +1,21 @@
+//variables (to check default jenkins variables - " localhost:8080/env-vars.html")
+CODE_CHANGES = getGitChanges()
+BRANCH = getGitBranch
+
 pipeline {
     agent {label ('linux-node')}
-    agent 'any'
-    agent 'none'
+    agent 'any' //OR 'none' 
+    environment {
+        NEW_VERSION = '1.3.0'      //these env variables will availble for all the stages
+        SERVER_CREDENTIALS = credentials('cred_ID') //available with credentials binding plugin
+    }
     stages {
         stage('stage-name') {
+            when {
+                expression {
+                    env.BRANCH == 'dev' || env.BRANCH == 'main'
+                }
+            }
             steps {
                 command_1 'Attributes'
                 command_2 'Attributes'
