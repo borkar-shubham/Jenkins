da51ece7d417b932ce4f73e1cea137da298a5cae
Shubham Borkar <shubham_borkar@outlook.com>
2025-02-08 22:41:08 +0530

    Create node-app.groovy

---
+++ Jenkinsfiles/node-app.groovy
@@ -0,0 +1,48 @@
+pipeline {
+    agent {
+        label ('cm-linux')
+    }
+    environment {
+        IMAGE_NAME = 'my-node-app'
+        IMAGE_TAG = 'latest'
+    }
+    stages {
+        stage('Clone Repository') {
+            steps {
+                git branch: 'main', url: 'https://github.com/borkar-shubham/nodeapp.git'
+            }
+        }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+                }
+            }
+        }
+
+        stage('Run Tests') {
+            steps {
+                script {
+                    sh "docker run --rm ${IMAGE_NAME}:${IMAGE_TAG} npm test"
+                }
+            }
+        }
+
+        stage('Push to Registry') {
+          when {
+              expression {
+                currentBuild.result == null || currentBuild.result == 'SUCCESS' 
+              }
+            }  
+          steps {
+                withDockerRegistry([credentialsId: 'docker-hub-credentials', url: 'https://index.docker.io/v1/']) {
+                    script {
+                        sh "docker tag ${IMAGE_NAME}:${IMAGE_TAG} shubhamborkar/${IMAGE_NAME}:${IMAGE_TAG}"
+                        sh "docker push shubhamborkar/${IMAGE_NAME}:${IMAGE_TAG}"
+                    }
+                }
+            }
+        }
+    }
+}
